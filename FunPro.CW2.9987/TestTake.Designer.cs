64181883f742ac3cc0e501b1de5a776dada34055
sjamoliddin <sjamolbek@mail.ru>
2021-03-16 23:05:05 +0500

    add TakeTest form

---
+++ FunPro.CW2.9987/TestTake.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace FunPro.CW2._9987
+{
+    partial class TestTake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbxAnswer3 = new System.Windows.Forms.TextBox();
+            this.lblQuestion3 = new System.Windows.Forms.Label();
+            this.lblQ3 = new System.Windows.Forms.Label();
+            this.tbxAnswer2 = new System.Windows.Forms.TextBox();
+            this.lblQuestion2 = new System.Windows.Forms.Label();
+            this.lblQ2 = new System.Windows.Forms.Label();
+            this.tbxAnswer1 = new System.Windows.Forms.TextBox();
+            this.lblQuestion1 = new System.Windows.Forms.Label();
+            this.lblQ1 = new System.Windows.Forms.Label();
+            this.cbxTestName = new System.Windows.Forms.ComboBox();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.lblApplicantName = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(212, 341);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 29);
+            this.btnCancel.TabIndex = 44;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(131, 341);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 29);
+            this.btnSave.TabIndex = 43;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // tbxAnswer3
+            // 
+            this.tbxAnswer3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxAnswer3.Location = new System.Drawing.Point(15, 303);
+            this.tbxAnswer3.Name = "tbxAnswer3";
+            this.tbxAnswer3.Size = new System.Drawing.Size(272, 22);
+            this.tbxAnswer3.TabIndex = 42;
+            // 
+            // lblQuestion3
+            // 
+            this.lblQuestion3.AutoSize = true;
+            this.lblQuestion3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion3.Location = new System.Drawing.Point(12, 278);
+            this.lblQuestion3.Name = "lblQuestion3";
+            this.lblQuestion3.Size = new System.Drawing.Size(99, 16);
+            this.lblQuestion3.TabIndex = 41;
+            this.lblQuestion3.Text = "Actual question";
+            // 
+            // lblQ3
+            // 
+            this.lblQ3.AutoSize = true;
+            this.lblQ3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQ3.Location = new System.Drawing.Point(12, 255);
+            this.lblQ3.Name = "lblQ3";
+            this.lblQ3.Size = new System.Drawing.Size(81, 16);
+            this.lblQ3.TabIndex = 40;
+            this.lblQ3.Text = "Question 3";
+            // 
+            // tbxAnswer2
+            // 
+            this.tbxAnswer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxAnswer2.Location = new System.Drawing.Point(15, 219);
+            this.tbxAnswer2.Name = "tbxAnswer2";
+            this.tbxAnswer2.Size = new System.Drawing.Size(272, 22);
+            this.tbxAnswer2.TabIndex = 39;
+            // 
+            // lblQuestion2
+            // 
+            this.lblQuestion2.AutoSize = true;
+            this.lblQuestion2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion2.Location = new System.Drawing.Point(12, 194);
+            this.lblQuestion2.Name = "lblQuestion2";
+            this.lblQuestion2.Size = new System.Drawing.Size(99, 16);
+            this.lblQuestion2.TabIndex = 38;
+            this.lblQuestion2.Text = "Actual question";
+            // 
+            // lblQ2
+            // 
+            this.lblQ2.AutoSize = true;
+            this.lblQ2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQ2.Location = new System.Drawing.Point(12, 171);
+            this.lblQ2.Name = "lblQ2";
+            this.lblQ2.Size = new System.Drawing.Size(81, 16);
+            this.lblQ2.TabIndex = 37;
+            this.lblQ2.Text = "Question 2";
+            // 
+            // tbxAnswer1
+            // 
+            this.tbxAnswer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxAnswer1.Location = new System.Drawing.Point(15, 138);
+            this.tbxAnswer1.Name = "tbxAnswer1";
+            this.tbxAnswer1.Size = new System.Drawing.Size(272, 22);
+            this.tbxAnswer1.TabIndex = 36;
+            // 
+            // lblQuestion1
+            // 
+            this.lblQuestion1.AutoSize = true;
+            this.lblQuestion1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion1.Location = new System.Drawing.Point(12, 113);
+            this.lblQuestion1.Name = "lblQuestion1";
+            this.lblQuestion1.Size = new System.Drawing.Size(99, 16);
+            this.lblQuestion1.TabIndex = 35;
+            this.lblQuestion1.Text = "Actual question";
+            // 
+            // lblQ1
+            // 
+            this.lblQ1.AutoSize = true;
+            this.lblQ1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQ1.Location = new System.Drawing.Point(12, 90);
+            this.lblQ1.Name = "lblQ1";
+            this.lblQ1.Size = new System.Drawing.Size(81, 16);
+            this.lblQ1.TabIndex = 34;
+            this.lblQ1.Text = "Question 1";
+            // 
+            // cbxTestName
+            // 
+            this.cbxTestName.DisplayMember = "TestName";
+            this.cbxTestName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxTestName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxTestName.FormattingEnabled = true;
+            this.cbxTestName.Location = new System.Drawing.Point(122, 43);
+            this.cbxTestName.Name = "cbxTestName";
+            this.cbxTestName.Size = new System.Drawing.Size(165, 24);
+            this.cbxTestName.TabIndex = 33;
+            this.cbxTestName.ValueMember = "TestName";
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTest.Location = new System.Drawing.Point(12, 46);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(35, 16);
+            this.lblTest.TabIndex = 32;
+            this.lblTest.Text = "Test";
+            // 
+            // lblApplicantName
+            // 
+            this.lblApplicantName.AutoSize = true;
+            this.lblApplicantName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApplicantName.Location = new System.Drawing.Point(119, 9);
+            this.lblApplicantName.Name = "lblApplicantName";
+            this.lblApplicantName.Size = new System.Drawing.Size(49, 16);
+            this.lblApplicantName.TabIndex = 31;
+            this.lblApplicantName.Text = "Name";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(12, 9);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(104, 16);
+            this.lbl.TabIndex = 30;
+            this.lbl.Text = "Applicant Name";
+            // 
+            // TestTake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 383);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbxAnswer3);
+            this.Controls.Add(this.lblQuestion3);
+            this.Controls.Add(this.lblQ3);
+            this.Controls.Add(this.tbxAnswer2);
+            this.Controls.Add(this.lblQuestion2);
+            this.Controls.Add(this.lblQ2);
+            this.Controls.Add(this.tbxAnswer1);
+            this.Controls.Add(this.lblQuestion1);
+            this.Controls.Add(this.lblQ1);
+            this.Controls.Add(this.cbxTestName);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.lblApplicantName);
+            this.Controls.Add(this.lbl);
+            this.Name = "TestTake";
+            this.Text = "TestTake";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tbxAnswer3;
+        private System.Windows.Forms.Label lblQuestion3;
+        private System.Windows.Forms.Label lblQ3;
+        private System.Windows.Forms.TextBox tbxAnswer2;
+        private System.Windows.Forms.Label lblQuestion2;
+        private System.Windows.Forms.Label lblQ2;
+        private System.Windows.Forms.TextBox tbxAnswer1;
+        private System.Windows.Forms.Label lblQuestion1;
+        private System.Windows.Forms.Label lblQ1;
+        private System.Windows.Forms.ComboBox cbxTestName;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Label lblApplicantName;
+        private System.Windows.Forms.Label lbl;
+    }
+}
---
+++ FunPro.CW2.9987/TestTake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunPro.CW2._9987
+{
+    public partial class TestTake : Form
+    {
+        public TestTake()
+        {
+            InitializeComponent();
+        }
+    }
+}
